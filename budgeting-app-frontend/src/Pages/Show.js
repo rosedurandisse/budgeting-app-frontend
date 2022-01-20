5161cb305c9dbfb2ed81ce50cbcf4245069c4819
Rose Durandisse <rosedurandisse@pursuit.org>
2022-01-20 16:24:46 -0500

    Included Transaction Details component

---
+++ budgeting-app-frontend/src/Pages/Show.js
@@ -0,0 +1,7 @@
+import TransactionDetails from "../Components/TransactionDetails";
+
+function ShowTransactionDetails() {
+  return <div>{TransactionDetails}</div>;
+}
+
+export default TransactionDetails;
