d06c72cadf9098b1b1f8c32d31c23d64b63b4606
Rose Durandisse <rosedurandisse@pursuit.org>
2022-01-20 12:17:06 -0500

    status version of the Index page

---
+++ budgeting-app-frontend/src/Pages/Index.js
@@ -0,0 +1,12 @@
+import Transactions from "../Components/Transactions";
+
+function Index() {
+  return (
+    <div>
+      <h2> All of the Transactions</h2>
+      <Transactions />
+    </div>
+  );
+}
+
+export default Index;
