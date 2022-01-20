eb3b52a00e7ae4cb043aab70f1b03cb945183375
Rose Durandisse <rosedurandisse@pursuit.org>
2022-01-20 16:25:23 -0500

    Updates to include show page

--- budgeting-app-frontend/src/App.js
+++ budgeting-app-frontend/src/App.js
@@ -3,14 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Pages
 import Home from "./Pages/Home";
 import Transactions from "./Pages/Index";
+import TransactionDetails from "./Components/TransactionDetails";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/transactions" element={<Transactions />}></Route>
+          <Route path="/" element={<Home />}>
+            {" "}
+          </Route>
+          <Route path="/transactions" element={<Transactions />}>
+            {" "}
+          </Route>
+          <Route path="/transactions/:index" element={<TransactionDetails />}>
+            {" "}
+          </Route>
         </Routes>
       </Router>
     </div>
