15e11bac1f7e27ec96f5e8c30fba9ae55b300456
Rose Durandisse <rosedurandisse@pursuit.org>
2022-01-20 17:16:08 -0500

    Added the nav bar

--- budgeting-app-frontend/src/App.js
+++ budgeting-app-frontend/src/App.js
@@ -3,23 +3,34 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Pages
 import Home from "./Pages/Home";
 import Transactions from "./Pages/Index";
-import TransactionDetails from "./Components/TransactionDetails";
+import TransactionDetails from "./Pages/Show";
+import TransactionEdit from "./Pages/Edit";
+
+//import the NacNar
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />}>
-            {" "}
-          </Route>
-          <Route path="/transactions" element={<Transactions />}>
-            {" "}
-          </Route>
-          <Route path="/transactions/:index" element={<TransactionDetails />}>
-            {" "}
-          </Route>
-        </Routes>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />}>
+              {" "}
+            </Route>
+            <Route path="/transactions" element={<Transactions />}>
+              {" "}
+            </Route>
+            <Route path="/transactions/:index" element={<TransactionDetails />}>
+              {" "}
+            </Route>
+            <Route
+              path="/transactions/:index/edit"
+              element={<TransactionEdit />}
+            ></Route>
+          </Routes>
+        </main>
       </Router>
     </div>
   );
