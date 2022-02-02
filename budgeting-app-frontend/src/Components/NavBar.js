8c72ed5c05aca6d361d58829d2b7869e8d77b25a
Rose Durandisse <rosedurandisse@pursuit.org>
2022-02-02 14:47:33 -0500

    Updating Nav component and images

--- budgeting-app-frontend/src/Components/NavBar.js
+++ budgeting-app-frontend/src/Components/NavBar.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import "../Styles/NavBar.css";
 
-export default function NavBar() {
+export default function NavBar(props) {
   return (
     <nav>
       <button>
